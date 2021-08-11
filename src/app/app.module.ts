ccfd27b86371adc30a8793c46cf8d13ea5da7918
vscalcione <vincenzo99.scalcione@gmail.com>
2021-08-11 09:45:26 +0200

    Update project: added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddDialogComponent } from './components/add-dialog/add-dialog.component';
+import { EditDialogComponent } from './components/edit-dialog/edit-dialog.component';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDialogComponent,
+    EditDialogComponent,
+    DeleteDialogComponent
   ],
   imports: [
     BrowserModule,
